b5890fc3b849f59c83f095b06a5650e74fd641a6
Rafał Tymiński <rafalt@cubiware.com>
2016-01-26 10:12:11 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,18 @@
+
+public class Client {
+	String firstName;
+	String lastName;
+	String pesel;
+	
+	String getFirstName() {
+		return this.firstName;
+	}
+	
+	void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	String getLastName() {
+		return this.lastName;
+	}
+}
